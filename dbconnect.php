b8903fbafc9ee0c81c8bd1f7c2fbf3434b358ac4
1409173 <chris49311@hotmail.co.uk>
2016-11-28 16:23:23 +0000

    connecting the database

---
+++ dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_SERVER','br-cdbr-azure-south-b.cloudapp.net');
+define('DB_USERNAME','b5e790d9e1fbf9');
+define('DB-PASSWORD','8768166b');
+define('DB_DATABASE','chrisdickdatabase');
+
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
